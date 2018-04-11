080da11c67dacb1b830bfdcc8d703a011364268d
Alex Mcauley <alex@nodex.co.uk>
2018-04-11 09:58:30 +0100

    Build.sh update

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker build -t nodextechnology/elasticsearch:2.4-alpine docker/elasticsearch/2.4
+docker build -t nodextechnology/elasticsearch:1.7.6-alpine docker/elasticsearch/1.7.6
+
+docker push nodextechnology/elasticsearch:2.4-alpine
+docker push nodextechnology/elasticsearch:1.7.6-alpine
