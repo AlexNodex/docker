bdf6e1ee863a315da5d1d7d021dbe6d1f154f7f9
Alex Mcauley <alex@nodex.co.uk>
2018-09-14 09:17:35 +0100

    ]0.0.0.0

--- docker/elasticsearch/2.4.6/docker-entrypoint.sh
+++ docker/elasticsearch/2.4.6/docker-entrypoint.sh
@@ -1,4 +1,6 @@
+
 #!/bin/bash
+
 set -e
 
 # Add elasticsearch as command if needed
@@ -10,8 +12,15 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of user-mutable directories to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R elasticsearch:elasticsearch "$path"
+	done
+
 	set -- su-exec elasticsearch "$@"
+	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
 fi
 
 # As argument is not related to elasticsearch,
