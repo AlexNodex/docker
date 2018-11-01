bb90657f6a71cc652ef1a2d9c97cb2ceb54fb65c
Alex Mcauley <alex@nodex.co.uk>
2018-11-01 13:03:43 +0000

    Build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t nodextechnology/elasticsearch:2.4.6-alpine docker/elasticsearch/2.4.6-alpine-delete
+docker build -t nodextechnology/elasticsearch:2.4.6-alpine-delete docker/elasticsearch/2.4.6-alpine-delete
 docker build -t nodextechnology/elasticsearch:1.7.6-alpine docker/elasticsearch/1.7.6-alpine
 
 docker push nodextechnology/elasticsearch:2.4.6-alpine-delete
