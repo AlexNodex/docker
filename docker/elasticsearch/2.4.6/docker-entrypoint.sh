a4c1e288016e94eeb05ca0948f6c43e5539be5c6
Alex Mcauley <alex@nodex.co.uk>
2019-09-18 10:11:41 +0100

    Overall ES directory permission

--- docker/elasticsearch/2.4.6/docker-entrypoint.sh
+++ docker/elasticsearch/2.4.6/docker-entrypoint.sh
@@ -12,6 +12,7 @@ fi
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of user-mutable directories to elasticsearch
 	for path in \
+		/usr/share/elasticsearch \
 		/usr/share/elasticsearch/data \
 		/usr/share/elasticsearch/logs \
 		/usr/share/elasticsearch/snapshots \
